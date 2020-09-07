9f5c1ae237e68c4fbc42e3c1df4183ab5c655d47
ehardwick <e_hardwick@coloradocollege.edu>
2020-09-07 14:48:20 -0400

    spaghetti 🤌 removal from UI

--- src/Client/UiTestMain.java
+++ src/Client/UiTestMain.java
@@ -1,41 +1,74 @@
 package Client;
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
+
+import java.awt.Container;
 import java.awt.GridBagLayout;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 import java.awt.GridBagConstraints;
 
-public class UiTestMain {
-  public static void main(String[] args) {
-     JFrame frame = new JFrame();
-     frame.setTitle("Messaging Client UI Test");
-     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    
-     SentMessagesPanel sentMessagesPanel = new SentMessagesPanel();
-     InputPanel inputPanel = new InputPanel();
-     ChatsPanel chatsPanel = new ChatsPanel(sentMessagesPanel, inputPanel);
-     ChatTitlePanel chatTitlePanel = new ChatTitlePanel();
-     chatsPanel.setMaximumSize(new Dimension(10, 10));
-     chatsPanel.setBounds(0, 0, 30, 100);
-    
-     JPanel test = new JPanel();
-     
-     GridBagConstraints c = new GridBagConstraints();
-     c.gridwidth = 1;
-    
-     test.setLayout(new BorderLayout());
-     test.add(sentMessagesPanel, BorderLayout.CENTER);
-     test.add(inputPanel, BorderLayout.SOUTH);
-     test.add(chatTitlePanel, BorderLayout.NORTH);
-    
-     frame.setLayout(new GridBagLayout());
-     frame.add(chatsPanel, c);
-     frame.add(test);
-    
-     frame.pack();
-     frame.setVisible(true);
+public class UiTestMain extends JFrame {
+  // GridBag garbage
+  private GridBagLayout grid;
+  private GridBagConstraints gbc;
+  private GridBagLayout layout;
 
+  // Different Panels
+  SentMessagesPanel sentMessagesPanel;
+  InputPanel inputPanel;
+  ChatsPanel chatsPanel;
+  ChatTitlePanel chatTitlePanel;
+
+  public static void main(String[] args) {
+    UiTestMain uiTest = new UiTestMain();
+  }
+
+  public UiTestMain() {
+    this.grid = new GridBagLayout();
+    this.gbc = new GridBagConstraints();
+    setLayout(grid);
+    setTitle("Messaging Client UI Test");
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.layout = new GridBagLayout();
+    this.setLayout(layout);
+
+    // initialize the panels
+    this.sentMessagesPanel = new SentMessagesPanel();
+    this.inputPanel = new InputPanel();
+    this.chatsPanel = new ChatsPanel(sentMessagesPanel, inputPanel);
+    this.chatTitlePanel = new ChatTitlePanel();
+    
+    test(this.getContentPane());
+
+  }
+
+  public void test(Container pane) {
+ 
+    // 🤌 spaghetti 🤌
+    gbc.fill = GridBagConstraints.VERTICAL;
+    gbc.gridx = 0;
+    gbc.gridy = 0;
+    gbc.gridwidth = 1;
+    gbc.gridheight = 6;
+    this.add(chatsPanel, gbc);
+    gbc.gridx = 1;
+    gbc.gridy = 0;
+    gbc.gridwidth = 3;
+    gbc.gridheight = 1;
+    gbc.fill = GridBagConstraints.HORIZONTAL;
+    this.add(chatTitlePanel, gbc);
+    gbc.gridx = 1;
+    gbc.gridy = 1;
+    gbc.gridwidth = 3;
+    gbc.gridheight = 4;
+    gbc.fill = GridBagConstraints.BOTH;
+    this.add(sentMessagesPanel, gbc);
+    gbc.gridx = 1;
+    gbc.gridy = 6;
+    gbc.gridwidth = 3;
+    gbc.gridheight = 1;
+    gbc.fill = GridBagConstraints.HORIZONTAL;
+    this.add(inputPanel, gbc);
+
+    pack();
+    setVisible(true);
   }
 }
