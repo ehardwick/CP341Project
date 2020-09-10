a08e1310301937baf4f85cdd7ec9187d3c0125c3
ehardwick <e_hardwick@coloradocollege.edu>
2020-09-10 16:18:37 -0400

    added the list of owners to the chat title

--- src/ui/ChatTitlePanel.java
+++ src/ui/ChatTitlePanel.java
@@ -1,35 +1,46 @@
 package ui;
 
 import java.util.Optional;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import util.MessageThread;
 
 @SuppressWarnings("serial")
 public class ChatTitlePanel extends JPanel implements MessageThreadObserver {
-  private static final String DEFAULT_CHAT_TITLE = "Chat Title";
+  private String chatTitle = "Chat Title";
+  private String chatOwners = "";
   private LocalStorage localStorage;
-  private JTextArea textArea;
+  private JTextArea titleText;
 
   public ChatTitlePanel(LocalStorage localStorage) {
     this.localStorage = localStorage;
-    this.textArea = new JTextArea(DEFAULT_CHAT_TITLE);
-    textArea.setOpaque(false);
-    textArea.setEditable(false);
-    textArea.setFocusable(false);
+    this.titleText = new JTextArea(chatTitle + "\n" + chatOwners);
 
-    add(textArea);
+    titleText.setOpaque(false);
+    titleText.setEditable(false);
+    titleText.setFocusable(false);
+    titleText.setAlignmentX(CENTER_ALIGNMENT);
+    
+    add(titleText);
   }
 
   @Override
   public void addNewMessageThread(MessageThread newThread) {
-    // nothing yet will want to switch automaticaly
+    // nothing yet will want to switch automatically
 
   }
 
   @Override
   public void threadSwitched(long messageThreadId) {
     Optional<MessageThread> switchedToThread = localStorage.getMessageThreadById(messageThreadId);
-    switchedToThread.ifPresent(thread -> textArea.setText(thread.getName()));
+    switchedToThread.ifPresent(thread -> {
+      StringBuilder owners = new StringBuilder();
+      chatTitle = thread.getName();
+      thread.getOwners().forEach(owner -> owners.append(owner.getUsername() + " "));
+      chatOwners = owners.toString();
+      titleText.setText(chatTitle + "\n" + chatOwners);
+    });
   }
 }
