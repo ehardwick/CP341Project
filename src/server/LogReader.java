a1842a1745ae66cb7b7dedbb09ef71e9a4db7c53
ehardwick <e_hardwick@coloradocollege.edu>
2020-09-14 23:07:15 -0700

    i missed another one

--- src/server/LogReader.java
+++ src/server/LogReader.java
@@ -1,7 +1,6 @@
 package server;
 
 import com.google.gson.Gson;
-
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.PrintWriter;
@@ -20,72 +19,74 @@ public class LogReader {
   private Gson gson = new Gson();
 
   public Map<Long, MessageThread> getMessageThreads() {
-	  try {
-		messageThreadsLog = gson.fromJson(new FileReader("MessageThreadsLog.txt"), MessageThreadsLog.class);
-	} catch (Exception e) {
-		throw new RuntimeException("Failed to Get Message Threads", e);
-	}
-	  return messageThreadsLog.getMap();
+    try {
+      messageThreadsLog =
+          gson.fromJson(new FileReader("MessageThreadsLog.txt"), MessageThreadsLog.class);
+    } catch (Exception e) {
+      throw new RuntimeException("Failed to Get Message Threads", e);
+    }
+    return messageThreadsLog.getMap();
   }
-  
+
   public void saveMessageThreads(MessageThreadsLog messageThreadsLog) {
-	  while(true) {
-		  PrintWriter out;
-			try {
-				out = new PrintWriter("MessageThreadsLog.txt");
-				out.println(gson.toJson(messageThreadsLog, MessageThreadsLog.class));
-				out.close();
-				break;
-			} catch (FileNotFoundException e) {
-				throw new RuntimeException("Failed to Save Users", e);
-			}  
-	  }
+    while (true) {
+      PrintWriter out;
+      try {
+        out = new PrintWriter("MessageThreadsLog.txt");
+        out.println(gson.toJson(messageThreadsLog, MessageThreadsLog.class));
+        out.close();
+        break;
+      } catch (FileNotFoundException e) {
+        throw new RuntimeException("Failed to Save Users", e);
+      }
+    }
   }
-  
+
   public Map<String, User> getUsers() {
-	  try {
-		userLog = gson.fromJson(new FileReader("UserLog.txt"), UserLog.class);
-	} catch (Exception e) {
-		throw new RuntimeException("Failed to Get Users", e);
-	}
-	  return userLog.getMap();
+    try {
+      userLog = gson.fromJson(new FileReader("UserLog.txt"), UserLog.class);
+    } catch (Exception e) {
+      throw new RuntimeException("Failed to Get Users", e);
+    }
+    return userLog.getMap();
   }
-  
-  
+
+
   public void saveUsers(UserLog userLog) {
-	  while(true) {
-		  PrintWriter out;
-			try {
-				out = new PrintWriter("UserLog.txt");
-				out.println(gson.toJson(userLog, UserLog.class));
-				out.close();
-				break;
-			} catch (FileNotFoundException e) {
-				throw new RuntimeException("Failed to Save Users", e);
-			}  
-	  }
+    while (true) {
+      PrintWriter out;
+      try {
+        out = new PrintWriter("UserLog.txt");
+        out.println(gson.toJson(userLog, UserLog.class));
+        out.close();
+        break;
+      } catch (FileNotFoundException e) {
+        throw new RuntimeException("Failed to Save Users", e);
+      }
+    }
   }
 
   public Map<String, List<MessageThread>> getUserMessageThreads() {
-	  try {
-		  userMessageThreadsLog = gson.fromJson(new FileReader("UserMessageThreadsLog.txt"), UserMessageThreadsLog.class);
-		} catch (Exception e) {
-			throw new RuntimeException("Failed to Get User Message Threads", e);
-		}
-		  return userMessageThreadsLog.getMap();
+    try {
+      userMessageThreadsLog =
+          gson.fromJson(new FileReader("UserMessageThreadsLog.txt"), UserMessageThreadsLog.class);
+    } catch (Exception e) {
+      throw new RuntimeException("Failed to Get User Message Threads", e);
+    }
+    return userMessageThreadsLog.getMap();
   }
-  
+
   public void saveUserMessageThreads(UserMessageThreadsLog userMessageThreadsLog) {
-	  while(true) {
-		  PrintWriter out;
-			try {
-				out = new PrintWriter("UserMessageThreadsLog.txt");
-				out.println(gson.toJson(userMessageThreadsLog, UserMessageThreadsLog.class));
-				out.close();
-				break;
-			} catch (FileNotFoundException e) {
-				System.out.println("retrying race condition");
-			}
-	  }
-  }  
+    while (true) {
+      PrintWriter out;
+      try {
+        out = new PrintWriter("UserMessageThreadsLog.txt");
+        out.println(gson.toJson(userMessageThreadsLog, UserMessageThreadsLog.class));
+        out.close();
+        break;
+      } catch (FileNotFoundException e) {
+        System.out.println("retrying race condition");
+      }
+    }
+  }
 }
